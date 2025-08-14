f70d091153370373fd87c1bf6952f5c45b868610
mayan.dev <mayangabriel654@gmail.com>
2025-08-14 17:24:53 -0300

    adição de fatos

---
+++ fatos.pl
@@ -0,0 +1,12 @@
+homem(mayan).
+homem(benjamim).
+homem(francisco).
+tio(francisco, benjamim).
+tio(francisco, mayan).
+mulher(margarida).
+mulher(gicelda).
+namorada(margarida, mayan).
+mae(gicelda, mayan).
+mae(gicelda, benjamim).
+irmao(mayan, benjamim).
+irmao(francisco, gicelda).
