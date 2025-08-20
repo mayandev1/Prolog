799da7f33eeff3330f263c160e3fd8db3a8ff6c3
mayan.dev <mayangabriel654@gmail.com>
2025-08-20 14:33:02 -0300

    feat: Adição de mais Fatos 20/08

--- fatos.pl
+++ fatos.pl
@@ -1,12 +1,33 @@
 homem(mayan).
 homem(benjamim).
 homem(francisco).
-tio(francisco, benjamim).
-tio(francisco, mayan).
+homem(pedro).
+homem(queilan).
+homem(luis).
+homem(thiago).
+homem(gladyston).
+
 mulher(margarida).
 mulher(gicelda).
-namorada(margarida, mayan).
+mulher(maria).
+mulher(dalva).
+mulher(leidivania).
+
+pai(gladyston, mayan).
+pai(gladyston, benjamim).
+pai(francisco, thiago).
+pai(luis, margarida).
+
+tio(francisco, mayan).
+tio(francisco, benjamim).
+
+tia(leidivania, mayan).
+tia(leidivania, benjamim).
+tia(gicelda, thiago).
+
 mae(gicelda, mayan).
 mae(gicelda, benjamim).
+mae(leidivania, thiago).
+
 irmao(mayan, benjamim).
 irmao(francisco, gicelda).
